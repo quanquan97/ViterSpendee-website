3b363370d72631518a97a45cdc039611c362050e
Will <fangzijian1997@hotmail.com>
2021-01-13 20:09:15 +0800

    deploy

---
+++ precache-manifest.bdb375eb751ff92c65dc60e7c055383e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57d07e0c47b333a600e8",
+    "url": "/ViterSpendee-website/css/app.60efbe31.css"
+  },
+  {
+    "revision": "974294194fe36c9a74cdf3bf6bb33fef",
+    "url": "/ViterSpendee-website/index.html"
+  },
+  {
+    "revision": "57d07e0c47b333a600e8",
+    "url": "/ViterSpendee-website/js/app.ff7bf99e.js"
+  },
+  {
+    "revision": "cf71564ecaf8171147b1",
+    "url": "/ViterSpendee-website/js/chunk-vendors.f403ff18.js"
+  },
+  {
+    "revision": "56c87b9035f3741fc84e2a6b02525480",
+    "url": "/ViterSpendee-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ViterSpendee-website/robots.txt"
+  }
+]);
